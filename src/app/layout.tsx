aabb195b2e74f03f42f55edb3daefc949bbb5412
Sabri KAŞ <sabri.kas@yandex.com>
2026-04-09 10:11:02 +0300

    Update font settings in globals.css and layout.tsx to use Noto Serif instead of Geist Sans for improved typography.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono, Noto_Serif } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const notoSerif = Noto_Serif({
+  variable: "--font-noto-serif",
   subsets: ["latin"],
 });
 
@@ -25,9 +25,11 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${notoSerif.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={`${notoSerif.className} min-h-full flex flex-col`}>
+        {children}
+      </body>
     </html>
   );
 }
